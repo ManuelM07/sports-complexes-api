6eaab114e8c75895a406ae80571d0b6903b33718
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2022-12-14 22:59:31 -0500

    Create mutations & querys

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,7 +5,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/ManuelM07/sports-complexes-api/graph/generated"
@@ -42,14 +41,50 @@ func (r *mutationResolver) CreateComplex(ctx context.Context, input model.Comple
 
 // CreateSchedule is the resolver for the createSchedule field.
 func (r *mutationResolver) CreateSchedule(ctx context.Context, input model.ScheduleInput) (*model.Schedule, error) {
-	panic(fmt.Errorf("not implemented: CreateSchedule - createSchedule"))
+	s := `INSERT INTO public.schedule(
+		start, "end")
+		VALUES ($1, $2) returning id, CAST(start AS string), CAST("end" AS string);`
+
+	resp, err := insertSchedule(s, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp, nil
+}
+
+// CreateScheduleComplex is the resolver for the createScheduleComplex field.
+func (r *mutationResolver) CreateScheduleComplex(ctx context.Context, input model.ScheduleComplexInput) (*model.ScheduleComplex, error) {
+	s := `INSERT INTO public.schedule_complex(
+		schedule_id, complex_id, available, limit_people, count_people)
+		VALUES ($1, $2, $3, $4, $5) returning *;`
+
+	resp, err := insertScheduleComplex(s, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp, nil
+}
+
+// CreateUserComplex is the resolver for the createUserComplex field.
+func (r *mutationResolver) CreateUserComplex(ctx context.Context, input model.UserComplexInput) (*model.UserComplex, error) {
+	s := `INSERT INTO public.user_complex(
+		user_id, complex_id)
+		VALUES ($1, $2) returning *;`
+
+	resp, err := insertUserComplex(s, input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp, nil
 }
 
 // User is the resolver for the user field.
 func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error) {
 	s := `select * from public.user where public.user.id = $1;`
 
-	//connectDB()
 	resp, err := getUser(s, id)
 	if err != nil {
 
@@ -60,12 +95,26 @@ func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error
 
 // Complex is the resolver for the complex field.
 func (r *queryResolver) Complex(ctx context.Context, id string) (*model.Complex, error) {
-	panic(fmt.Errorf("not implemented: Complex - complex"))
+	s := `select * from public.complex where public.complex.id = $1;`
+
+	resp, err := getComplex(s, id)
+	if err != nil {
+
+		log.Fatal(err)
+	}
+	return resp, nil
 }
 
 // Schedule is the resolver for the schedule field.
 func (r *queryResolver) Schedule(ctx context.Context, id string) (*model.Schedule, error) {
-	panic(fmt.Errorf("not implemented: Schedule - schedule"))
+	s := `select * from public.schedule where public.schedule.id = $1;`
+
+	resp, err := getSchedule(s, id)
+	if err != nil {
+
+		log.Fatal(err)
+	}
+	return resp, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
