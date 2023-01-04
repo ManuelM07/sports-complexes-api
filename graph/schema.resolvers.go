6c46c26e47bbfa1dfe49a1da9dfad51032589771
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2023-01-03 20:38:04 -0500

    Create new resolver
    Created new resolver to ScheduleComplex and update sintaxis

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,6 +5,7 @@ package graph
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/ManuelM07/sports-complexes-api/graph/generated"
@@ -15,7 +16,7 @@ import (
 func (r *mutationResolver) CreateUser(ctx context.Context, input model.UserInput) (*model.User, error) {
 	s := `INSERT INTO public.user(
 			name, years, birthday, weight, height)
-			VALUES ($1, $2, $3, $4, $5) returning *;`
+			VALUES ($1, $2, $3, $4, $5) RETURNING *;`
 
 	resp, err := insertUser(s, input)
 	if err != nil {
@@ -29,7 +30,7 @@ func (r *mutationResolver) CreateUser(ctx context.Context, input model.UserInput
 func (r *mutationResolver) CreateComplex(ctx context.Context, input model.ComplexInput) (*model.Complex, error) {
 	s := `INSERT INTO public.complex(
 			name)
-			VALUES ($1) returning *;`
+			VALUES ($1) RETURNING *;`
 
 	resp, err := insertComplex(s, input)
 	if err != nil {
@@ -57,7 +58,7 @@ func (r *mutationResolver) CreateSchedule(ctx context.Context, input model.Sched
 func (r *mutationResolver) CreateScheduleComplex(ctx context.Context, input model.ScheduleComplexInput) (*model.ScheduleComplex, error) {
 	s := `INSERT INTO public.schedule_complex(
 		schedule_id, complex_id, available, limit_people, count_people)
-		VALUES ($1, $2, $3, $4, $5) returning *;`
+		VALUES ($1, $2, $3, $4, $5) RETURNING *;`
 
 	resp, err := insertScheduleComplex(s, input)
 	if err != nil {
@@ -71,7 +72,7 @@ func (r *mutationResolver) CreateScheduleComplex(ctx context.Context, input mode
 func (r *mutationResolver) CreateUserComplex(ctx context.Context, input model.UserComplexInput) (*model.UserComplex, error) {
 	s := `INSERT INTO public.user_complex(
 		user_id, complex_id)
-		VALUES ($1, $2) returning *;`
+		VALUES ($1, $2) RETURNING *;`
 
 	resp, err := insertUserComplex(s, input)
 	if err != nil {
@@ -83,7 +84,7 @@ func (r *mutationResolver) CreateUserComplex(ctx context.Context, input model.Us
 
 // User is the resolver for the user field.
 func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error) {
-	s := `select * from public.user where public.user.id = $1;`
+	s := `SELECT * FROM public.user WHERE public.user.id = $1;`
 
 	resp, err := getUser(s, id)
 	if err != nil {
@@ -95,7 +96,7 @@ func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error
 
 // Complex is the resolver for the complex field.
 func (r *queryResolver) Complex(ctx context.Context, id string) (*model.Complex, error) {
-	s := `select * from public.complex where public.complex.id = $1;`
+	s := `SELECT * FROM public.complex WHERE public.complex.id = $1;`
 
 	resp, err := getComplex(s, id)
 	if err != nil {
@@ -107,7 +108,7 @@ func (r *queryResolver) Complex(ctx context.Context, id string) (*model.Complex,
 
 // Schedule is the resolver for the schedule field.
 func (r *queryResolver) Schedule(ctx context.Context, id string) (*model.Schedule, error) {
-	s := `select id, CAST(start AS string), CAST("end" AS string) from public.schedule where public.schedule.id = $1;`
+	s := `SELECT id, CAST(start AS string), CAST("end" AS string) FROM public.schedule where public.schedule.id = $1;`
 
 	resp, err := getSchedule(s, id)
 	if err != nil {
@@ -119,7 +120,7 @@ func (r *queryResolver) Schedule(ctx context.Context, id string) (*model.Schedul
 
 // Users is the resolver for the users field.
 func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
-	s := `select * from public.user;`
+	s := `SELECT * FROM public.user;`
 
 	resp, err := getUsers(s)
 	if err != nil {
@@ -131,7 +132,7 @@ func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
 
 // Complexs is the resolver for the complexs field.
 func (r *queryResolver) Complexs(ctx context.Context) ([]*model.Complex, error) {
-	s := `select * from public.complex;`
+	s := `SELECT * FROM public.complex;`
 
 	resp, err := getComplexs(s)
 	if err != nil {
@@ -143,7 +144,7 @@ func (r *queryResolver) Complexs(ctx context.Context) ([]*model.Complex, error) 
 
 // Schedules is the resolver for the schedules field.
 func (r *queryResolver) Schedules(ctx context.Context) ([]*model.Schedule, error) {
-	s := `select id, CAST(start AS string), CAST("end" AS string) from public.schedule;`
+	s := `SELECT id, CAST(start AS string), CAST("end" AS string) FROM public.schedule;`
 
 	resp, err := getSchedules(s)
 	if err != nil {
@@ -151,6 +152,32 @@ func (r *queryResolver) Schedules(ctx context.Context) ([]*model.Schedule, error
 		log.Fatal(err)
 	}
 	return resp, nil
+}
+
+// ScheduleComplex is the resolver for the scheduleComplex field.
+func (r *queryResolver) ScheduleComplex(ctx context.Context, complexID string, available *bool) ([]*model.ScheduleComplex, error) {
+	s := `SELECT p.id, p.schedule_id, p.complex_id, p.available, p.limit_people, p.count_people, 
+	s.id, CAST(s.start AS string), CAST(s."end" AS string) 
+	FROM public.schedule_complex p
+	JOIN schedule s
+		ON s.id = p.schedule_id AND p.complex_id = $1;`
+
+	resp, err := getScheduleComplex(s, complexID, available)
+	if err != nil {
+
+		log.Fatal(err)
+	}
+	return resp, nil
+}
+
+// UserComplexToUser is the resolver for the userComplexToUser field.
+func (r *queryResolver) UserComplexToUser(ctx context.Context, userID string) ([]*model.UserComplex, error) {
+	panic(fmt.Errorf("not implemented: UserComplexToUser - userComplexToUser"))
+}
+
+// UserComplexToComplex is the resolver for the userComplexToComplex field.
+func (r *queryResolver) UserComplexToComplex(ctx context.Context, complexID string) ([]*model.UserComplex, error) {
+	panic(fmt.Errorf("not implemented: UserComplexToComplex - userComplexToComplex"))
 }
 
 // Mutation returns generated.MutationResolver implementation.
